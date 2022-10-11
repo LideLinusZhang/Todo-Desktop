2eeaba6f7bd5acb3c64bed80ae271b8361a2ab76
JiaoyangXu <59169603+JiaoyangXu@users.noreply.github.com>
2022-10-11 01:01:12 -0400

    add initial fxml panel

--- src/main/kotlin/com/example/todo_desktop/ui/MainView.kt
+++ src/main/kotlin/com/example/todo_desktop/ui/MainView.kt
@@ -1,24 +1,20 @@
 package com.example.todo_desktop.ui
 import javafx.scene.Parent
+import javafx.scene.layout.BorderPane
 import tornadofx.*
 import java.awt.Button
 import java.awt.Label
 
 class MainView: View() {
-    val listView: ListView by inject<ListView>()
+    //val listView: ListView by inject<ListView>()
 
-    override val root = borderpane {
+    override val root : BorderPane by fxml("main_view.fxml")
+
+    init {
+        root.setPrefSize(1200.0, 720.0)
         title = "Anywhere ToDo"
-        setPrefSize(1200.0, 720.0)
-        top = listView.root
     }
 }
 
-class ListView: View() {
 
-    override val root = vbox {
-        button("Click me") {  }
-        label("Hello World") {  }
-    }
-}
 
