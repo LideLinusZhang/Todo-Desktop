0c76539f0d24df17090adc1f408954870806bdd4
Mingzhou Yue <m9yue@uwaterloo.ca>
2022-10-12 23:01:05 -0400

    Experiment title label styles

--- src/main/kotlin/com/example/todo_desktop/ui/SubjectListView.kt
+++ src/main/kotlin/com/example/todo_desktop/ui/SubjectListView.kt
@@ -1,5 +1,7 @@
 package com.example.todo_desktop.ui
-
+import javafx.geometry.Pos.CENTER
+import javafx.geometry.Pos.CENTER_RIGHT
+import javafx.geometry.Pos.CENTER_LEFT
 import javafx.beans.property.SimpleStringProperty
 import tornadofx.*
 
@@ -8,12 +10,21 @@ class SubjectListView : View("Subject List") {
     private var records = mutableListOf<String>().observable()
     val input = SimpleStringProperty()
     override val root = vbox {
+        vbox {
+            alignment = CENTER
+            label("SUBJECTS") {
+                style = "-fx-font: 20 arial;"
+                minWidth(50.0)
+                minHeight(50.0)
+            }
+        }
         listview(records) {
             cellFormat {
                 text = it
             }
         }
         form {
+            alignment = CENTER_RIGHT
             fieldset {
                 field("Enter Subject Name") {
                     textfield(input)
@@ -26,7 +37,6 @@ class SubjectListView : View("Subject List") {
                 }
             }
         }
-
     }
     init {
         records.add("CS 346")
