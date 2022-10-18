360cd4f1e3dccf6a66ef6b009adef215eb7603d6
Jiaoyang Xu <jiaoyang.xu@uwaterloo.ca>
2022-10-17 22:45:42 -0400

    Issue 23

--- src/main/kotlin/com/example/todo_desktop/data/ToDoInfo.kt
+++ src/main/kotlin/com/example/todo_desktop/data/ToDoInfo.kt
@@ -2,10 +2,11 @@ package com.example.todo_desktop.data
 
 import java.time.LocalDate
 
-class ToDoInfo(val info: String = "", val prio: Int = 3, val date: LocalDate = LocalDate.now() ) {
+class ToDoInfo(val info: String = "", val prio: Int = 3, val date: LocalDate = LocalDate.now(), val fav: Boolean = false ) {
     var author = ""
     var content = info
     var priority = prio
     var dueDate = date
     var isCompleted = false
+    var isStared = fav
 }
